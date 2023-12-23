3b2ef197e05f3f6de059e88a733fae20f270dbe4
noodle-ing <nonomi2603@gmail.com>
2023-12-23 23:03:00 +0600

    Add WebApi application in Api and add UserController

---
+++ User.Api/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using Users.Application.DTO;
+using Users.Application.Services;
+
+namespace User.Api.Controllers;
+
+public class UsersController : ControllerBase
+{
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var result = await _userService.GetAllAsync();
+        return Ok(result);
+    }
+
+    [HttpGet("{id:long}")]
+    public async Task<IActionResult> Get([FromRoute] long id)
+    {
+        var result = await _userService.GetAsync(id);
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody]CreateUserDto dto)
+    {
+        var result = await _userService.CreateAsync(dto);
+        return Ok(result);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update([FromBody] UpdateUserDto dto)
+    {
+        var result = await _userService.UpdateAsync(dto);
+        return Ok(result);
+    }
+    
+    [HttpDelete("{id:long}")]
+    public async Task<IActionResult> Delete([FromBody] long id)
+    {
+        var result = await _userService.DeleteAsync(id);
+        return Ok(result);
+    }
+    
+}
+}
